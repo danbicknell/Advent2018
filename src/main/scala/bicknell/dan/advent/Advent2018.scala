03cf873bfaeed939bfb0a5613879436a568831db
Daniel Bicknell <Daniel.Bicknell@genmills.com>
2018-12-13 07:51:22 -0600

    Advent 1b

--- src/main/scala/bicknell/dan/advent/Advent2018.scala
+++ src/main/scala/bicknell/dan/advent/Advent2018.scala
@@ -1,22 +1,23 @@
 package bicknell.dan.advent
 
-object Advent2018 extends App{
-
+object Advent2018 extends App {
 
 
   override def main(args: Array[String]): Unit = {
     println("Hello Advent")
+    /*
     val one = new One
     val result = one.sumSequence
     println("The answer to Puzzle 1-A is: " + result)
     val result2 = one.repeatingFrequency
     println("The answer to Puzzle 1-B is: " + result2)
+*/
+    val two = new Two
+    //val result2a = two.calculateChecksum
+    //println("The answer to Puzzle 2-A is: " + result2a)
 
-
-
-
-
-
+    val result2b = two.detectSimilarBoxes
+    println("The answer to Puzzle 2-B is: " + result2b)
 
 
   }
---
+++ src/main/scala/bicknell/dan/advent/Two.scala
@@ -0,0 +1,86 @@
+package bicknell.dan.advent
+
+import scala.collection.immutable.NumericRange
+import scala.collection.mutable.ArrayBuffer
+
+class Two {
+
+  val BoxIds = readBoxIdFile
+  val Letters: NumericRange.Inclusive[Char] = 'a' to 'z'
+
+  println("Two initialized - there are " + BoxIds.length + " Box IDs")
+
+
+  def calculateChecksum: Int = {
+    var doubleLetters = 0
+    var trippleLetters = 0
+
+    for (box <- BoxIds) {
+      doubleLetters += checkForDoubles(box)
+      trippleLetters += checkForTriples(box)
+    }
+
+    (doubleLetters * trippleLetters)
+  }
+
+  def detectSimilarBoxes: String = {
+    val testOne = "abcde"
+    val testTwo = "abcdf"
+    var matchNumber = 0
+
+    for (box1 <- BoxIds) {
+      for (box2 <- BoxIds) {
+
+        matchNumber = computeStringCharacterMatches(box1, box2)
+        if (matchNumber == 1) return box2 intersect box1
+
+      }
+
+    }
+
+
+    println("The number of non-matching characters is: " + matchNumber)
+
+    "No matches found"
+
+  }
+
+
+  def readBoxIdFile: ArrayBuffer[String] = {
+    import scala.io.Source
+    val result = ArrayBuffer[String]()
+    println("opening file")
+    val filename = "C:\\TempWork\\Advent\\Advent2.txt"
+    for (line <- Source.fromFile(filename).getLines) {
+      result += line
+    }
+
+    result
+
+  }
+
+  def checkForDoubles(box: String): Int = {
+
+    for (letter <- Letters) {
+      val count = box.count(_ == letter)
+      if (count == 2) return 1
+    }
+
+    0
+  }
+
+  def checkForTriples(box: String): Int = {
+    for (letter <- Letters) {
+      val count = box.count(_ == letter)
+      if (count == 3) return 1
+    }
+    0
+  }
+
+  def computeStringCharacterMatches(str1: String, str2: String): Int = {
+    if (str1.length != str2.length)
+      throw new IllegalArgumentException("Strings have different length")
+    (0 to str1.length - 1).filter(i => str1(i) == str2(i)).size
+  }
+
+}
